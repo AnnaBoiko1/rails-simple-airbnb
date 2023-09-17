3279bd2e1b833b5231b6a7e02d3107eb509b36d0
Anna Boiko <annaboiko1@icloud.com>
2023-09-17 19:15:39 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+20.times do
+  Flat.create!(
+    name: Faker::App.name,
+    address: Faker::Address.full_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: (50..300).sample,
+    number_of_guests: (1..5).sample
+  )
+end
